87b78e2f64e36ed1f04d71b045fcaf68507eda37
Mateus Henrique <mateushenriquechaves1@gmail.com>
2023-04-27 01:29:16 -0300

    chore: prisma settings

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
-export const app = fastify();
+export const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Alice',
+    email: 'mateus@gmail.com',
+  },
+})
+
+export const app = fastify()
